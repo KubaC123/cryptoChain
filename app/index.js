722664e69befaa15d3c3472f461f126df495907c
Jakub Cichy <jakubcichy@MacBook-Pro-Jakub.local>
2018-09-11 10:45:00 +0200

    update

--- app/index.js
+++ app/index.js
@@ -1,4 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('../blockChain');
+const P2Pserver = require('./p2p-server');
 
-const HTTP_PORT = 3001;
+const HTTP_PORT = process.env.HTTP_PORT || 3001; // user can specify port on the command line
+
+const app = express();
+const bc = new Blockchain();
+const p2pServer = new P2Pserver(bc);
+
+app.use(bodyParser.json()); // allows to recieve json within post request
+
+app.get('/blocks', (request, response) =>{
+    response.json(bc.chain);
+});
+
+app.post('/mine', (request, response) => {
+    const block = bc.addBlock(request.body.data);
+    console.log(`new block added: ${block.toString()}`);
+
+    response.redirect('/blocks');
+});
+
+app.listen(HTTP_PORT, () => console.log(`listening on port ${HTTP_PORT}`));
+p2pServer.listen();
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -19,11 +19,36 @@ class P2Pserver {
     listen() { 
         const server = new Websocket.Server({ port: P2P_PORT });
         server.on('connection', socket => this.connectSocket(socket));
+
+        this.connectToPeers();
+
         console.log(`Listening for p2p connections on ${P2P_PORT}`);
+    }
+
+    connectToPeers() { 
+        peers.forEach(peer => {
+            // ws://localhost:5001 ...
+            const socket = new Websocket(peer);
+
+            socket.on('open', () => this.connectSocket(socket));
+        });
     }
 
     connectSocket(socket) {
         this.sockets.push(socket);
         console.log('Socket connected.');
+
+        this.messageHandler(socket);
+
+        socket.send(JSON.stringify(this.blockchain.chain));
+        }
+
+    messageHandler(socket) {
+        socket.on('message', message => {
+            const data = JSON.parse(message);
+            console.log('data', data);
+        });
     }
 }
+
+module.exports = P2Pserver;
